9076357b6f4d8e74e8ed892961e3a2fdc3e5c21b
Gagan Kapoor <gaganKapoor092485@gmail.com>
2025-04-16 16:50:05 +0530

    Responsive changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
@@ -10,10 +10,20 @@ import Videos from './components/Videos'
 function App() {
 
   const [url, setUrl] = useState('/');
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 640)
 
   const handleUrl=(link) =>{
     setUrl(link);
   }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640);
+    };
+  
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
 
   return (
@@ -21,8 +31,8 @@ function App() {
     <div className='wrapper'>
         <div className='left_section'>
               <ul>
-                  <li className={url==='/' ? 'active' : ''} onClick={()=>handleUrl('/')}><Link to="/">Dashboard</Link></li>
-                  <li className={url==='/videos' ? 'active' : ''}  onClick={()=>handleUrl('/videos')}><Link to="/videos">Videos</Link></li>
+                  <li className={url==='/' ? 'active' : ''} onClick={()=>handleUrl('/')}><Link to="/">{isMobile ? 'D' : 'Dashboard'}</Link></li>
+                  <li className={url==='/videos' ? 'active' : ''}  onClick={()=>handleUrl('/videos')}><Link to="/videos">{isMobile ? 'V' : 'Videos'}</Link></li>
               </ul>
         </div>
         <div className='right_section'>
